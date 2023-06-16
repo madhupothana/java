4047a278297c26b80b71f1c0dace0012d4cd8d74
madhupothana <136293906+madhupothana@users.noreply.github.com>
2023-06-16 12:01:56 +0530

    Update ticket.java

--- ticket.java
+++ ticket.java
@@ -66,21 +66,3 @@ class Passenger{
 }	
 
 
-class Passage{
-	public static void main(String args[]){
-		String s=",madhu lovEs, madhu";
-		int sp=1,d=1,ch=0;
-		for(int i=0;i<s.length();i++){
-			if(s.charAt(i)==' ')
-				sp++;
-			else if(s.charAt(i)=='.'){
-				d++;ch++;
-			}
-			else
-			ch++;
-		}
-		System.out.println("THE NUMBER OF LINES "+d);
-		System.out.println("THE NUMBER OF CHARECTERS "+(ch));
-		System.out.println("THE NUMBER OF WORDS "+sp);
-	}
-}
