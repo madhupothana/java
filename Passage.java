c258bd49ea217c8f4b7d20e67c1dd0bed90d9d0d
madhupothana <136293906+madhupothana@users.noreply.github.com>
2023-08-01 16:01:20 +0530

    Update Passage.java

--- Passage.java
+++ Passage.java
@@ -1,18 +1,101 @@
-class Passage{
-	public static void main(String args[]){
-		String s=",madhu lovEs, madhu";
-		int sp=1,d=1,ch=0;
-		for(int i=0;i<s.length();i++){
-			if(s.charAt(i)==' ')
-				sp++;
-			else if(s.charAt(i)=='.'){
-				d++;ch++;
-			}
-			else
-			ch++;
-		}
-		System.out.println("THE NUMBER OF LINES "+d);
-		System.out.println("THE NUMBER OF CHARECTERS "+(ch));
-		System.out.println("THE NUMBER OF WORDS "+sp);
-	}
-}
+Explain the isolation level with flow of steps and syntaxes. 
+ChatGPT
+Isolation level is a concept in database management systems (DBMS) that defines the level of visibility and interaction between concurrent transactions. It ensures that the operations performed by one transaction are isolated from the operations of other concurrent transactions. Isolation levels provide different levels of data consistency, allowing developers to choose an appropriate level based on their application's requirements and potential trade-offs.
+
+There are four standard isolation levels defined by the SQL standard, and they are:
+
+Read Uncommitted
+Read Committed
+Repeatable Read
+Serializable
+Let's explain each isolation level with a flow of steps and provide the SQL syntax to set each isolation level:
+
+Read Uncommitted:
+
+In this isolation level, transactions are not isolated from each other, and dirty reads, non-repeatable reads, and phantom reads can occur.
+Dirty read: A transaction reads data that has been modified by another uncommitted transaction.
+Non-repeatable read: A transaction reads the same data multiple times, but the data changes between reads.
+Phantom read: A transaction reads a set of rows, but another transaction inserts or deletes rows, causing the first transaction to see a different set of rows when re-executed.
+Syntax to set Read Uncommitted isolation level in SQL Server:
+
+css
+Copy code
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+Read Committed:
+
+In this isolation level, transactions are isolated from each other, and dirty reads are prevented. However, non-repeatable reads and phantom reads can still occur.
+Dirty read is prevented, but a transaction may still read data that has been modified and committed by another transaction before the current transaction is completed.
+Syntax to set Read Committed isolation level in SQL Server:
+
+css
+Copy code
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+Repeatable Read:
+
+In this isolation level, transactions are isolated from each other, and both dirty reads and non-repeatable reads are prevented. However, phantom reads can still occur.
+A transaction can read the same data multiple times, and the data remains consistent between reads even if another transaction modifies and commits the data.
+Syntax to set Repeatable Read isolation level in SQL Server:
+
+css
+Copy code
+SET TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+Serializable:
+
+In this isolation level, transactions are completely isolated from each other, and dirty reads, non-repeatable reads, and phantom reads are all prevented.
+Serializable isolation level provides the highest level of data consistency but may result in increased blocking and decreased concurrency.
+Syntax to set Serializable isolation level in SQL Server:
+
+css
+Copy code
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;
+It's important to note that the specific syntax to set the isolation level may vary slightly depending on the database management system you are using. Additionally, some databases may have additional isolation levels or variations of these standard levels.
+
+
+
+
+User
+Explain the importance of Atomicity of a transaction with a real time example. Explain the
+concept behind commit and rollback of transaction with DML statements like insert, update &
+Delete.
+ChatGPT
+Importance of Atomicity of a Transaction with Real-Time Example:
+
+Atomicity is one of the ACID properties of a transaction in database management systems. It ensures that a transaction is treated as a single indivisible unit of work. The entire transaction must either complete successfully, resulting in a committed state, or be completely rolled back, leaving the database in its original state if any part of the transaction fails.
+
+To understand the importance of atomicity, let's consider a real-time example of a bank transfer:
+
+Suppose you want to transfer money from your bank account (Account A) to your friend's account (Account B). The bank application needs to perform the following steps as part of the transaction:
+
+Deduct the amount from your Account A.
+Add the same amount to your friend's Account B.
+Now, imagine that both steps are separate database operations within a transaction. If the transaction fails in the middle, it could lead to inconsistent data and financial loss. For example, let's say the transaction fails after deducting money from your Account A but before adding it to your friend's Account B.
+
+Without atomicity, you would be left with a situation where the money is deducted from your account, but your friend's account is not credited with the transferred amount. This would result in a financial discrepancy and an unsatisfied transaction, which is unacceptable in a banking system.
+
+Atomicity ensures that either both steps are executed successfully and the transaction is committed, or none of the steps are executed, and the transaction is rolled back. This guarantees data consistency and integrity, ensuring that the money transfer is either fully completed or completely reverted, maintaining the correctness of the database.
+
+Commit and Rollback of Transactions with DML Statements:
+
+In database systems, Data Manipulation Language (DML) statements are used to modify data in the database. The two essential operations related to transactions and DML statements are COMMIT and ROLLBACK:
+
+COMMIT:
+
+COMMIT is a DML statement used to make the changes performed by a transaction permanent and visible to other transactions.
+When a transaction is committed, all the changes made within that transaction are saved to the database, and the transaction is completed.
+After the COMMIT statement is executed, the transaction is no longer active, and its changes are durable.
+Syntax for COMMIT:
+sql
+Copy code
+COMMIT;
+ROLLBACK:
+
+ROLLBACK is a DML statement used to undo all the changes made by a transaction and restore the database to its state before the transaction began.
+When a transaction is rolled back, all the changes made within that transaction are discarded, and the database returns to its previous state.
+ROLLBACK is typically used when an error occurs during a transaction, and the changes made should be reversed to maintain data consistency.
+Syntax for ROLLBACK:
+sql
+Copy code
+ROLLBACK;
+For example, let's consider a scenario where you are updating the price of a product in an online store. You start a transaction to update the price of a specific product. If there's an error or inconsistency in the update, you can use the ROLLBACK statement to revert the changes made during the transaction. On the other hand, if the update is successful and you want to save the changes, you can use the COMMIT statement to make the changes permanent and visible to other users of the system.
+
+Using COMMIT and ROLLBACK effectively helps maintain data integrity and consistency in the database, ensuring that only valid and complete transactions are committed to the database while any incomplete or erroneous transactions are rolled back to prevent data corruption.
